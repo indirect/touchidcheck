38f64493bdcb616052e6ceb1852ef441af86e512
Andre Arko <andre@arko.net>
2017-01-27 22:56:48 -0800

    touchidcheck implemented, hooray

--- touchidcheck/main.m
+++ touchidcheck/main.m
@@ -7,11 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <LocalAuthentication/LocalAuthentication.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    LAContext *myContext = [[LAContext alloc] init];
+    NSError *authError = nil;
+
+    if ([myContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&authError]) {
+        printf("Found a TouchID sensor!\n");
+        return 0;
+    } else {
+        printf("Could not find a TouchID sensor.\n");
+        return 1;
     }
-    return 0;
 }
